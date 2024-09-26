88029e990605fc3a1f16e505a5dbf2394048745e
Gustavo Quim <gquimb@miumg.edu.gt>
2024-09-25 23:36:20 -0600

    Creacion de vistas

--- bases/urls.py
+++ bases/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-
+#importa el HOME de las rutas
+from bases.views import Home
 urlpatterns = [
-    
+    #ingreso de ruta como vista, con nombre home
+    path('',Home.as_view(),name='home'),
 ]
