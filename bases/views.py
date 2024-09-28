b3d533f26e08e56daa7559a104a0530d33dc9a5b
Gustavo Quim <gquimb@miumg.edu.gt>
2024-09-27 19:02:54 -0600

    Implementación de sistema de autenticación Mixin

--- bases/views.py
+++ bases/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
-# Create your views here.
 
-class Home(generic.TemplateView):
+# Create your views here.
+# Herencia de la vista
+class Home(LoginRequiredMixin, generic.TemplateView):
     template_name = 'bases/home.html'
+    login_url='/admin'
